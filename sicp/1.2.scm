7f5aef87661903829065edce53c0b37776aacbcf
yffu <5543388+yffu@users.noreply.github.com>
2026-05-24 15:01:33 -0700

    Update file extention to .scm

--- sicp/1.2.scm
+++ sicp/1.2.scm
@@ -86,4 +86,21 @@ iterative process, as state is captured by (dec a) and (inc b) and their sum. on
 (pascal 3 2)
 (pascal 5 3)
 
-;1.13
+;1.14
+(define (count-change amount) (cc amount 5))
+(define (cc amount kinds-of-coins)
+(cond ((= amount 0) 1)
+((or (< amount 0) (= kinds-of-coins 0)) 0)
+(else (+ (cc amount
+(- kinds-of-coins 1))
+(cc (- amount
+(first-denomination
+kinds-of-coins))
+kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+(cond ((= kinds-of-coins 1) 1)
+((= kinds-of-coins 2) 5)
+((= kinds-of-coins 3) 10)
+((= kinds-of-coins 4) 25)
+((= kinds-of-coins 5) 50)))
+
