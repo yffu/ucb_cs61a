45bcb341c865eb191b75e0268b15675766cb50df
yffu <5543388+yffu@users.noreply.github.com>
2026-06-13 14:38:17 -0700

    Add 1.18

--- sicp/1.17.scm
+++ sicp/1.17.scm
@@ -21,3 +21,16 @@
 (mult 2 4)
 
 (fast-mult 3 7)
+
+; 1.18
+; 2 * 6 > 4 * 3 > 4 * 1 + 4 * 2 > 4 * 2 + n, where n is 1 * 4
+(define (fast-mult-iter a b n)
+  (cond ((= b 1) (+ a n))
+        ((= (remainder b 2) 0) (fast-mult-iter (double a) (halve b) n))
+        (else (fast-mult-iter a (- b 1) (+ n a)))))
+
+(fast-mult-iter 3 7 0)
+(fast-mult-iter 1001 1001 0)
+
+                                            
+  
