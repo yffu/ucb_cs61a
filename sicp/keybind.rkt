da67a85b177d60dd2865ce61bde8fb30e26b1d21
yffu <5543388+yffu@users.noreply.github.com>
2026-06-19 12:23:44 -0700

    Add keybind for comment shortcut

---
+++ sicp/keybind.rkt
@@ -0,0 +1,8 @@
+#lang s-exp framework/keybinding-lang
+; DrRacket custom keybind. Edit > Keybindings > Add User-defined Keybindings > Select this file > OKs
+
+;; Bind Ctrl + / to Comment Out Selection
+(keybinding "c:/" (λ (editor event) (send editor comment-out-selection)))
+
+;; Bind Ctrl + Shift + / to Uncomment Selection
+(keybinding "c:s:/" (λ (editor event) (send editor uncomment-selection)))
