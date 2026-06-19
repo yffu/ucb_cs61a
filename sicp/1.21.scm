406825ba8983e046453e7a8cf808253b0cb238b6
yffu <5543388+yffu@users.noreply.github.com>
2026-06-19 12:23:20 -0700

    Add 1.25

--- sicp/1.21.scm
+++ sicp/1.21.scm
@@ -136,3 +136,22 @@
 
 ; due to the fermat method choosing a random number the runtime does not always trend up with increased n
 
+; 1.25
+; expmod and fast-expt are similar in that they both reduce the input by half, resulting in Theta(log(n)) steps
+; but expmod is more efficient as it carries out the remainder operation after each recursive call and reduces the size of operands
+; the fast-expt version forces the raminder operation until the very end and force fast-expt to compute very large intermediate numbers.
+(define (fast-expt b n )
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+;(define (expmod base exp m)
+;  (cond ((= exp 0) 1)
+;        ((even? exp)
+;         (remainder
+;          (square (expmod base (/ exp 2) m))
+;          m))
+;        (else
+;         (remainder
+;          (* base (expmod base (- exp 1) m))
+;          m))))
