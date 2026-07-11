2e5ea2a17e7eab84bce1769dc783b51363675848
yffu <5543388+yffu@users.noreply.github.com>
2026-07-10 21:17:45 -0700

    Add 1.31b

--- sicp/1.31.scm
+++ sicp/1.31.scm
@@ -9,6 +9,11 @@
         (iter (next a) (* result (term a)))))
   (iter a 1))
 
+(define (product-rec term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product-rec term (next a) next b))))
+
 (define (identity x) x)
 (define (inc n) (+ n 1))
 
@@ -21,12 +26,18 @@
   (define (pi-term x)
     (define numer (* 2 (quotient (+ x 3) 2)))
     (define denom (+ (* 2 (quotient (+ x 2) 2)) 1))
-;    (display numer)
-;    (display "/")
-;    (display denom)
-;    (newline)
     (/ numer denom))
   (* (product pi-term 0 inc n) 4.0))
 
+(define (pi-prod-rec n)
+  (define (pi-term x)
+    (define numer (* 2 (quotient (+ x 3) 2)))
+    (define denom (+ (* 2 (quotient (+ x 2) 2)) 1))
+    (/ numer denom))
+  (* (product-rec pi-term 0 inc n) 4.0))
+
 (pi-prod 100)
 (pi-prod 1000)
+
+(pi-prod-rec 100)
+(pi-prod-rec 1000)
