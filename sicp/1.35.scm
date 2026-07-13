27e8809bfba5d78b506e4d3d4cd818c6e5cdcefe
yffu <5543388+yffu@users.noreply.github.com>
2026-07-12 17:53:02 -0700

    Add 1.35

---
+++ sicp/1.35.scm
@@ -0,0 +1,18 @@
+#lang sicp
+
+;1.35
+
+(define tolerance 0.0001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+; phi ~ 1.6180
