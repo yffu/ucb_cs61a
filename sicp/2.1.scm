dffa2dbcdd35003573b89f9b7f2a7e5e88570032
yffu <5543388+yffu@users.noreply.github.com>
2026-08-16 13:53:05 -0700

    Add 2.1

---
+++ sicp/2.1.scm
@@ -0,0 +1,65 @@
+#lang sicp
+
+;2.1
+
+; cons: construct
+(define x (cons 1 2))
+
+; car : contents of address part of register
+(car x)
+
+; cdr: contents of decrement part of register
+(cdr x)
+
+(define (make-rat n d)
+  (let ((g (gcd n d))
+        (n (if (< (* n d) 0) (* -1 (abs n)) (abs n)))
+        (d (abs d)))
+    (cons (/ n g) (/ d g))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
+(print-rat (make-rat -1 3))
+
+(print-rat (make-rat -1 -3))
+
+(print-rat (make-rat 1 -3))
+
+(print-rat (make-rat 1 3))
