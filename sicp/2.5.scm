e981af69ec5ba22f3b7df4594795dc3779eb59fc
yffu <5543388+yffu@users.noreply.github.com>
2026-08-17 16:35:55 -0700

    Add 2.5

---
+++ sicp/2.5.scm
@@ -0,0 +1,29 @@
+#lang sicp
+
+; 2.5
+
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define pair (cons 2 3))
+
+(define (car x)
+  (define (iter x n)
+    (if (= (modulo x 2) 0)
+        (iter (/ x 2) (+ n 1))
+        n))
+  (iter x 0))
+
+(define (cdr x)
+  (define (iter x n)
+    (if (= (modulo x 3) 0)
+        (iter (/ x 3) (+ n 1))
+        n))
+  (iter x 0))
+
+(car pair)
+
+(cdr pair)
+
+
+
