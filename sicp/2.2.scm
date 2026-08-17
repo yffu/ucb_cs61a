b709d8ed4bde23bfd84fd93f1d8bc8171fab7660
yffu <5543388+yffu@users.noreply.github.com>
2026-08-17 13:19:44 -0700

    Add 2.3

--- sicp/2.2.scm
+++ sicp/2.2.scm
@@ -34,9 +34,53 @@
   (display (x-point p))
   (display ",")
   (display (y-point p))
-  (display ")"))
+  (display ")")
+  (newline))
 
 (define s1
   (make-segment (make-point 0 0) (make-point 4 4)))
 
 (print-point (midpoint-segment s1))
+
+; 2.3
+
+(define (make-rect s1 s2)
+  (cons s1 s2))
+
+(define (rect-s1 rect)
+  (car rect))
+
+(define (rect-s2 rect)
+  (cdr rect))
+
+(define (make-vector segment)
+  (let ((p1 (start-segment segment))
+        (p2 (end-segment segment)))
+    (make-point (- (x-point p2) (x-point p1))
+                (- (y-point p2) (y-point p1)))))
+
+(define (length segment)
+  (let ((p1 (start-segment segment))
+        (p2 (end-segment segment)))
+    (sqrt (+ (expt (- (x-point p2) (x-point p1)) 2)
+             (expt (- (y-point p2) (y-point p1)) 2)))))
+
+    
+(define (area-rect rect)
+  (let ((v1 (make-vector (rect-s1 rect)))
+        (v2 (make-vector (rect-s2 rect))))
+    (abs (- (* (x-point v1) (y-point v2))
+            (* (x-point v2) (y-point v1))))))
+
+(define (perimeter-rect rect)
+  (let ((l1 (length (rect-s1 rect)))
+        (l2 (length (rect-s2 rect))))
+    (* (+ l1 l2) 2)))
+
+(define r1
+  (make-rect
+   (make-segment (make-point 0 0) (make-point 0 2))
+   (make-segment (make-point 0 0) (make-point 4 0))))
+
+(area-rect r1)
+(perimeter-rect r1)
